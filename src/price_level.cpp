ad2437259250a7c4613ff36c110fa07ab81697d5
benf-dojo <ben.faulkner@paymentsense.com>
2025-10-26 22:43:11 +0000

    fix: set head when tail null

--- src/price_level.cpp
+++ src/price_level.cpp
@@ -1,19 +1,32 @@
 #include "price_level.hpp"
 
-void PriceLevel::push_back(Order* o) {
+void PriceLevel::push_back(Order *o)
+{
     o->prev = tail;
     o->next = nullptr;
     o->level = this;
-    if (tail) tail->next = o;
+    if (tail)
+    {
+        tail->next = o;
+    }
+    else
+    {
+        head = o;
+    }
     tail = o;
     total_qty += o->qty;
 }
 
-void PriceLevel::remove(Order* o){
-    if(o->prev) o->prev->next = o->next;
-    else head = o->next;
-    if(o->next) o->next->prev = o->prev;
-    else tail = o->prev;
+void PriceLevel::remove(Order *o)
+{
+    if (o->prev)
+        o->prev->next = o->next;
+    else
+        head = o->next;
+    if (o->next)
+        o->next->prev = o->prev;
+    else
+        tail = o->prev;
     total_qty -= o->qty;
     o->prev = o->next = nullptr;
     o->level = nullptr;
